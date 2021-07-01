73496d384e8ca43d2c2d3a05283bb6cf2dffdeb6
Andrew Hall <andrew.hall@blinkhealth.com>
2021-07-01 00:57:24 -0400

    WIP

--- controller.py
+++ controller.py
@@ -18,6 +18,21 @@ def write_s3_object(client, s3_path: str, data: str) -> None:
     assert result["ResponseMetadata"]["HTTPStatusCode"] == 200
 
 
+def read_s3_object(client, s3_path: str) -> str:
+    path_components = s3_path.split("/")
+    assert len(path_components) == 2
+    bucket = path_components[0]
+    path = path_components[1]
+
+    result = client.get_object(
+        Bucket=bucket,
+        Key=path,
+    )
+    assert result["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    return str(result["Body"].read(), "UTF-8")
+
+
 @click.group()
 def cli():
     pass
