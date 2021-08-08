8e5496750a7e9043df05daece441fcc583f9f27e
Sean Gibat <sean.aucremanne@gmail.com>
2021-08-08 17:27:33 -0400

    Fix some bugs

--- controller.py
+++ controller.py
@@ -40,23 +40,23 @@ class S3LockClient():
     P0_TURN_VALUE = "0"
     P1_TURN_VALUE = "1"
     
-    def __init__(self, s3_client, process_num: int, bucket: str, wait_time_s: int = 1, verbose: bool = False):
+    def __init__(self, s3_client, process_num: str, bucket: str, wait_time_s: int = 1, verbose: bool = False):
         self.s3_client = s3_client
         self.process_num = process_num
         self.bucket = bucket
         self.verbose = verbose
 
     def wait_for_lock(self: str):
-        if verbose:
+        if self.verbose:
             print(f"I am the mighty process {self.process_num}!")
             print(f"Setting {self._my_flag_file} flag and setting turn to {self._their_turn_value}.")
 
-        write_s3_object(self._my_flag_file, self.FLAG_SET_VALUE)
-        write_s3_object(self.TURN_FILE, self._their_turn_value)
+        self._write_s3_object(self._my_flag_file, self.FLAG_SET_VALUE)
+        self._write_s3_object(self.TURN_FILE, self._their_turn_value)
         
         started = datetime.datetime.now()
         while (self._they_want_to_enter and self._its_their_turn):
-            if verbose:
+            if self.verbose:
                 waited_for = (datetime.datetime.now() - started).seconds
                 print(f"Poor little old me, process {self.process_num}, waiting for my turn for {waited_for} seconds..")
 
@@ -64,7 +64,7 @@ class S3LockClient():
             time.sleep(self.wait_time_s)
 
         # My turn! Woooooooooooooo
-        if verbose:
+        if self.verbose:
             print(f"Acquired lock for process {self.process_num}!")
 
     @property
@@ -77,18 +77,18 @@ class S3LockClient():
 
     @property
     def _my_flag_file(self):
-        return FLAG_0_FILE if self.process_num == 0 else FLAG_1_FILE
+        return self.FLAG_0_FILE if self.process_num == "0" else self.FLAG_1_FILE
 
     @property
     def _their_flag_file(self):
-        return FLAG_0_FILE if self.process_num == 1 else FLAG_1_FILE
+        return self.FLAG_0_FILE if self.process_num == "1" else self.FLAG_1_FILE
 
     @property
     def _their_turn_value(self):
-        return P0_TURN_VALUE if self.process_num == 1 else P1_TURN_VALUE
+        return self.P0_TURN_VALUE if self.process_num == "1" else self.P1_TURN_VALUE
 
     def _write_s3_object(self, filename, data: str) -> None:
-        result = self.client.put_object(
+        result = self.s3_client.put_object(
             Body=data,
             Bucket=self.bucket,
             Key=filename,
@@ -96,7 +96,7 @@ class S3LockClient():
         assert result["ResponseMetadata"]["HTTPStatusCode"] == 200
 
     def _read_s3_object(self, filename) -> str:
-        result = self.client.get_object(
+        result = self.s3_client.get_object(
             Bucket=self.bucket,
             Key=filename,
         )
@@ -111,9 +111,9 @@ def cli():
 
 @cli.command()
 @click.option("--bucket", "-b", required=True, type=str, help="Bucket")
-@click.option("--process-num", "-p", required=True, type=click.Choice([0, 1]), help="Process number")
-@click.option("--verbose", default=False, is_flag=True)
-def acquire_lock(bucket: str, process_num: int, verbose: bool = False) -> None:
+@click.option("--process-num", "-p", required=True, type=click.Choice(["0", "1"]), help="Process number")
+@click.option("--verbose", "-v", default=False, is_flag=True)
+def acquire_lock(bucket: str, process_num: str, verbose: bool = False) -> None:
     """Acquire lock"""
 
     s3_client = boto3.client("s3")
@@ -125,3 +125,5 @@ def acquire_lock(bucket: str, process_num: int, verbose: bool = False) -> None:
         verbose=verbose,
     ).wait_for_lock()
 
+if __name__ == '__main__':
+    cli()
